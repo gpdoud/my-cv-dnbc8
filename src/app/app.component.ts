db9b427fb3f1fa65519352ea9703ed410974bb8b
Greg Doud <gpdoud@gmail.com>
2020-03-19 13:54:42 -0400

    added description at top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  description: string = "1 component";
   name: string = "Greg Doud";
   address: string = "4900 Parkway Dr.";
   city: string = "Mason";
